ccd41534a343736ac9f50a8c9d5cf063921e6a96
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-10-03 22:42:44 -0700

    Add FourWheelRandom, our first implementation.
    Also BitExtensions with some important methods.

--- ShaiRandom/IEnhancedRandom.cs
+++ ShaiRandom/IEnhancedRandom.cs
@@ -3,6 +3,24 @@ using System.Collections.Generic;
 
 namespace ShaiRandom
 {
+    public static class BitExtensions
+    {
+        /// <summary>
+        /// Bitwise left-rotation of a ulong by amt, in bits.
+        /// </summary>
+        /// <param name="ul">The ulong to rotate left.</param>
+        /// <param name="amt">How many bits to rotate.</param>
+        /// <returns>The rotated ul.</returns>
+        public static ulong RotateLeft(this ulong ul, int amt) => (ul << amt) | (ul >> 64 - amt);
+
+        /// <summary>
+        /// Bitwise right-rotation of a ulong by amt, in bits.
+        /// </summary>
+        /// <param name="ul">The ulong to rotate right.</param>
+        /// <param name="amt">How many bits to rotate.</param>
+        /// <returns>The rotated ul.</returns>
+        public static ulong RotateRight(this ulong ul, int amt) => (ul >> amt) | (ul << 64 - amt);
+    }
     public interface IEnhancedRandom
     {
         private static readonly float FLOAT_ADJUST = MathF.Pow(2f, -24f);
