9165fc2fc824d92d0622b8d7347513a50d58664f
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-01-04 18:36:42 -0800

    Fix and update docs in FourWheelRandom.

--- ShaiRandom/Generators/FourWheelRandom.cs
+++ ShaiRandom/Generators/FourWheelRandom.cs
@@ -3,8 +3,12 @@
 namespace ShaiRandom.Generators
 {
     /// <summary>
-    /// It's an AbstractRandom with 4 states, more here later.
+    /// It's an AbstractRandom with 4 states, built around a "chaotic" construction with no guarantee of a minimum period, but likely to be a very long one.
     /// </summary>
+    /// <remarks>
+    /// This is very fast... on the JVM. It isn't nearly as fast on .NET (any version), because smaller state sizes do better here. It's still fairly high-quality,
+    /// with only one known failing test, and it only failed after 2 exabytes of data were processed (an extinction-saturation test).
+    /// </remarks>
     public class FourWheelRandom : AbstractRandom
     {
         /// <summary>
@@ -34,9 +38,9 @@ namespace ShaiRandom.Generators
         /// <remarks>If this has just been set to some value, then the next call to <see cref="NextULong()">NextUlong()</see> will return that value as-is. Later calls will be more random.</remarks>
         public ulong StateD { get; set; }
 
-        /**
-         * Creates a new FourWheelRandom with a random state.
-         */
+        /// <summary>
+        /// Creates a new FourWheelRandom with a random state.
+        /// </summary>
         public FourWheelRandom()
         {
             StateA = MakeSeed();
@@ -45,24 +49,28 @@ namespace ShaiRandom.Generators
             StateD = MakeSeed();
         }
 
-        /**
-         * Creates a new FourWheelRandom with the given seed; all {@code long} values are permitted.
-         * The seed will be passed to {@link #setSeed(long)} to attempt to adequately distribute the seed randomly.
-         * @param seed any {@code long} value
-         */
+        /// <summary>
+        /// Creates a new FourWheelRandom with the given seed; all ulong values are permitted.
+        /// </summary>
+        /// <remarks>
+        /// The seed will be passed to <see cref="SetSeed(FourWheelRandom, ulong)">SetSeed(FourWheelRandom, ulong)</see>, to attempt to adequately distribute the seed randomly.
+        /// </remarks>
+        /// <param name="seed">Any ulong.</param>
         public FourWheelRandom(ulong seed)
         {
             SetSeed(this, seed);
         }
 
-        /**
-         * Creates a new FourWheelRandom with the given four states; all {@code long} values are permitted.
-         * These states will be used verbatim.
-         * @param stateA any {@code long} value
-         * @param stateB any {@code long} value
-         * @param stateC any {@code long} value
-         * @param stateD any {@code long} value
-         */
+        /// <summary>
+        /// Creates a new FourWheelRandom with the given four states; all ulong values are permitted.
+        /// </summary>
+        /// <remarks>
+        /// The states will be used verbatim.
+        /// </remarks>
+        /// <param name="stateA">Any ulong.</param>
+        /// <param name="stateB">Any ulong.</param>
+        /// <param name="stateC">Any ulong.</param>
+        /// <param name="stateD">Any ulong.</param>
         public FourWheelRandom(ulong stateA, ulong stateB, ulong stateC, ulong stateD)
         {
             StateA = stateA;
@@ -91,12 +99,13 @@ namespace ShaiRandom.Generators
         /// This supports <see cref="PreviousULong()"/>.
         /// </summary>
         public override bool SupportsPrevious => true;
-        /**
-         * Gets the state determined by {@code selection}, as-is. The value for selection should be
-         * between 0 and 3, inclusive; if it is any other value this gets state D as if 3 was given.
-         * @param selection used to select which state variable to get; generally 0, 1, 2, or 3
-         * @return the value of the selected state
-         */
+
+        /// <summary>
+        /// Gets the state determined by selection, as-is.
+        /// </summary>
+        /// <remarks>The value for selection should be between 0 and 3, inclusive; if it is any other value this gets state D as if 3 was given.</remarks>
+        /// <param name="selection">used to select which state variable to get; generally 0, 1, 2, or 3.</param>
+        /// <returns>The value of the selected state.</returns>
         public override ulong SelectState(int selection)
         {
             switch (selection)
@@ -112,13 +121,14 @@ namespace ShaiRandom.Generators
             }
         }
 
-        /**
-         * Sets one of the states, determined by {@code selection}, to {@code value}, as-is.
-         * Selections 0, 1, 2, and 3 refer to states A, B, C, and D,  and if the selection is anything
-         * else, this treats it as 3 and sets stateD.
-         * @param selection used to select which state variable to set; generally 0, 1, 2, or 3
-         * @param value the exact value to use for the selected state, if valid
-         */
+        /// <summary>
+        /// Sets one of the states, determined by {@code selection}, to {@code value}, as-is.
+        /// </summary>
+        /// <remarks>
+        /// Selections 0, 1, 2, and 3 refer to states A, B, C, and D,  and if the selection is anything else, this treats it as 3 and sets stateD.
+        /// </remarks>
+        /// <param name="selection">Used to select which state variable to set; generally 0, 1, 2, or 3.</param>
+        /// <param name="value">The exact value to use for the selected state, if valid.</param>
         public override void SetSelectedState(int selection, ulong value)
         {
             switch (selection)
@@ -138,13 +148,14 @@ namespace ShaiRandom.Generators
             }
         }
 
-        /**
-         * This initializes all 4 states of the generator to random values based on the given seed.
-         * (2 to the 64) possible initial generator states can be produced here, all with a different
-         * first value returned by {@link #nextLong()} (because {@code stateD} is guaranteed to be
-         * different for every different {@code seed}).
-         * @param seed the initial seed; may be any long
-         */
+        /// <summary>
+        /// Initializes all 4 states of the generator to random values based on the given seed.
+        /// </summary>
+        /// <remarks>
+        /// (2 to the 64) possible initial generator states can be produced here, all with a different first value returned
+        /// by <see cref="NextULong()">NextUlong()</see> (because stateD is guaranteed to be different for every different seed).
+        /// </remarks>
+        /// <param name="seed">The initial seed; may be any ulong.</param>
         public override void Seed(ulong seed) => SetSeed(this, seed);
 
         private static void SetSeed(FourWheelRandom rng, ulong seed)
@@ -178,18 +189,18 @@ namespace ShaiRandom.Generators
             }
         }
 
-        /**
-         * Sets the state completely to the given four state variables.
-         * This is the same as calling {@link #setStateA(long)}, {@link #setStateB(long)},
-         * {@link #setStateC(long)}, and {@link #setStateD(long)} as a group. You may want
-         * to call {@link #nextLong()} a few times after setting the states like this, unless
-         * the value for stateD (in particular) is already adequately random; the first call
-         * to {@link #nextLong()}, if it is made immediately after calling this, will return {@code stateD} as-is.
-         * @param stateA the first state; can be any long
-         * @param stateB the second state; can be any long
-         * @param stateC the third state; can be any long
-         * @param stateD the fourth state; this will be returned as-is if the next call is to {@link #nextLong()}
-         */
+        /// <summary>
+        /// Sets the state completely to the given four state variables.
+        /// </summary>
+        /// <remarks>
+        /// This is the same as calling setStateA(ulong), setStateB(ulong), setStateC(ulong), and setStateD(ulong) as a group.
+        /// You may want to call <see cref="NextULong()">NextUlong()</see> a few times after setting the states like this, unless
+        /// the value for stateD (in particular) is already adequately random; the first call to NextULong(), if it is made immediately after calling this, will return stateD as-is.
+        /// </remarks>
+        /// <param name="stateA">The first state; can be any long.</param>
+        /// <param name="stateB">The second state; can be any long.</param>
+        /// <param name="stateC">The third state; can be any long.</param>
+        /// <param name="stateD">The fourth state; this will be returned as-is if the next call is to NextULong().</param>
         public override void SetState(ulong stateA, ulong stateB, ulong stateC, ulong stateD)
         {
             StateA = stateA;
