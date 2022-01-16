ea42b2b4112ddd8cafbb332ad4e81090172f5cb3
Chris3606 <cridley106@gmail.com>
2022-01-16 00:00:54 -0600

    Added unit tests for basic integer functions.

---
+++ ShaiRandom.UnitTests/KnownSeriesRandomTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ShaiRandom.Generators;
+using Xunit;
+
+namespace ShaiRandom.UnitTests
+{
+    public class KnownSeriesRandomTests
+    {
+        private const int LowerInt = 0;
+        private const int UpperInt = 10;
+
+        [Fact]
+        public void NextIntLowerBound()
+        {
+            var rng = new KnownSeriesRandom(new[] { LowerInt });
+
+            Assert.Equal(LowerInt, rng.NextInt(LowerInt + 1));
+            Assert.Equal(LowerInt, rng.NextInt(LowerInt, LowerInt + 1));
+
+            Assert.Throws<ArgumentException>(() => rng.NextInt(LowerInt + 1, LowerInt + 2));
+        }
+        
+        [Fact]
+        public void NextIntUpperBound()
+        {
+            var rng = new KnownSeriesRandom(new[] { UpperInt });
+
+            Assert.Equal(UpperInt, rng.NextInt(UpperInt + 1));
+            Assert.Equal(UpperInt, rng.NextInt(UpperInt, UpperInt + 1));
+
+            Assert.Throws<ArgumentException>(() => rng.NextInt(UpperInt));
+            Assert.Throws<ArgumentException>(() => rng.NextInt(UpperInt - 1, UpperInt));
+        }
+    }
+}
