e7d4c1319ef0c79db4c17e8ab25330fd21d406d2
Chris3606 <cridley106@gmail.com>
2022-01-16 22:35:54 -0600

    Used dynamic typing to create generic functions to unit test bounds for various types.

--- ShaiRandom.UnitTests/KnownSeriesRandomTests.cs
+++ ShaiRandom.UnitTests/KnownSeriesRandomTests.cs
@@ -11,6 +11,7 @@ namespace ShaiRandom.UnitTests
         private const int LowerValue = 0;
         private const int UpperValue = 10;
 
+        private static readonly float s_floatAdjust = MathF.Pow(2f, -24f);
         private static readonly double s_doubleCloseTo1 = 1 - Math.Pow(2, -53);
 
         private readonly KnownSeriesRandom _lowerRNG = new KnownSeriesRandom(
@@ -31,6 +32,16 @@ namespace ShaiRandom.UnitTests
             floatSeries: new []{(float)UpperValue},
             longSeries: new []{(long)UpperValue},
             ulongSeries: new []{(ulong)UpperValue}
+        );
+
+        private readonly KnownSeriesRandom _unboundedRNG = new KnownSeriesRandom(
+            new []{int.MinValue, int.MaxValue},
+            new []{uint.MinValue, uint.MaxValue},
+            new []{0.0, 1.0 - s_doubleCloseTo1},
+            byteSeries: new []{byte.MinValue, byte.MaxValue},
+            floatSeries: new []{0.0f, 1.0f - s_floatAdjust},
+            longSeries: new []{long.MinValue, long.MaxValue},
+            ulongSeries: new []{ulong.MinValue, ulong.MaxValue}
         );
 
         #region Double
@@ -79,52 +90,71 @@ namespace ShaiRandom.UnitTests
         }
         #endregion
 
+        #region Template Tests
+
+        private void TestUnboundedIntFunction<T>(Func<T> unboundedGenFunc)
+        {
+            T minValue = (T)typeof(T).GetField("MinValue")!.GetValue(null)!;
+            T maxValue = (T)typeof(T).GetField("MaxValue")!.GetValue(null)!;
+
+            Assert.Equal(minValue, unboundedGenFunc());
+            Assert.Equal(maxValue,unboundedGenFunc());
+        }
+
+        private static void TestLowerBoundIntFunction<T>(Func<T, T> upperBoundFunc, Func<T, T, T> dualBoundFunc)
+            where T : IConvertible
+        {
+            // Duck-type the generic type so that we can add/subtract from it using the type's correct operators.
+            dynamic value = (T)Convert.ChangeType(LowerValue, typeof(T));
+
+            Assert.Equal(value, upperBoundFunc(value + 1));
+            Assert.Equal(value, dualBoundFunc(value, value + 1));
+
+            Assert.Throws<ArgumentException>(() => dualBoundFunc(value + 1, value + 2));
+        }
+
+        private static void TestCrossedBoundIntFunction<T>(Func<T, T, T> generatorFunc)
+            where T : IConvertible
+        {
+            // Duck-type the generic type so that we can add/subtract from it using the type's correct operators.
+            dynamic value = (T)Convert.ChangeType(UpperValue, typeof(T));
+
+            // Allowed range: value
+            Assert.Equal(value, generatorFunc(value, value));
+            // Allowed range: value
+            Assert.Equal(value, generatorFunc(value, value - 1));
+            // Allowed range: [value - 1, value]
+            Assert.Equal(value, generatorFunc(value, value - 2));
+        }
+
+        private static void TestUpperBoundIntFunction<T>(Func<T, T> upperBoundFunc, Func<T, T, T> dualBoundFunc)
+            where T : IConvertible
+        {
+            // Duck-type the generic type so that we can add/subtract from it using the type's correct operators.
+            dynamic value = (T)Convert.ChangeType(UpperValue, typeof(T));
+
+            Assert.Equal(value, upperBoundFunc(value + 1));
+            Assert.Equal(value, dualBoundFunc(value, value + 1));
+
+            Assert.Throws<ArgumentException>(() => upperBoundFunc(value));
+            Assert.Throws<ArgumentException>(() => dualBoundFunc(value - 1, value));
+        }
+        #endregion
+
         #region Int
 
         [Fact]
-        public void NextIntUnbounded()
-        {
-            var rng = new KnownSeriesRandom(new[] { int.MinValue, int.MaxValue });
-
-            Assert.Equal(int.MinValue, rng.NextInt());
-            Assert.Equal(int.MaxValue,rng.NextInt());
-        }
+        public void NextIntUnbounded() => TestUnboundedIntFunction(_unboundedRNG.NextInt);
 
         [Fact]
-        public void NextIntLowerBound()
-        {
-            int value = LowerValue;
-
-            Assert.Equal(value, _lowerRNG.NextInt(value + 1));
-            Assert.Equal(value, _lowerRNG.NextInt(value, value + 1));
-
-            Assert.Throws<ArgumentException>(() => _lowerRNG.NextInt(value + 1, value + 2));
-        }
+        public void NextIntLowerBound() => TestLowerBoundIntFunction<int>(_lowerRNG.NextInt, _lowerRNG.NextInt);
 
         [Fact]
-        public void NextIntCrossedBounds()
-        {
-            int value = UpperValue;
-
-            // Allowed range: value
-            Assert.Equal(value, _upperRNG.NextInt(value, value));
-            // Allowed range: value
-            Assert.Equal(value, _upperRNG.NextInt(value, value - 1));
-            // Allowed range: [value - 1, value]
-            Assert.Equal(value, _upperRNG.NextInt(value, value - 2));
-        }
+        public void NextIntCrossedBounds() => TestCrossedBoundIntFunction<int>(_upperRNG.NextInt);
 
         [Fact]
-        public void NextIntUpperBound()
-        {
-            int value = UpperValue;
+        public void NextIntUpperBound() => TestUpperBoundIntFunction<int>(_upperRNG.NextInt, _upperRNG.NextInt);
 
-            Assert.Equal(value, _upperRNG.NextInt(value + 1));
-            Assert.Equal(value, _upperRNG.NextInt(value, value + 1));
-
-            Assert.Throws<ArgumentException>(() => _upperRNG.NextInt(value));
-            Assert.Throws<ArgumentException>(() => _upperRNG.NextInt(value - 1, value));
-        }
         #endregion
 
     }
