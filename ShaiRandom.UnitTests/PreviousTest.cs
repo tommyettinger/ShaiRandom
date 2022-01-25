d3039f5ff69ed596b309bf08e9b7072303b3e575
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-01-24 21:10:40 -0800

    Add a real test for PreviousULong().

---
+++ ShaiRandom.UnitTests/PreviousTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ShaiRandom.Generators;
+using Xunit;
+
+namespace ShaiRandom.UnitTests
+{
+    /// <summary>
+    /// Tests which check to make sure the generator implementations that support PreviousULong() do so correctly.
+    /// </summary>
+    public class PreviousTests
+    {
+        [Fact]
+        public void PreviousMatchesTest()
+        {
+            IEnhancedRandom[] generators = DataGenerators.CreateGenerators(true).Where(g => g.SupportsPrevious).ToArray();
+            foreach (var gen in generators)
+            {
+                List<ulong> forward = new List<ulong>(100);
+                for (int i = 0; i < 100; i++)
+                {
+                    forward.Add(gen.NextULong());
+                }
+                gen.NextULong();
+                forward.Reverse();
+                for (int i = 0; i < 100; i++)
+                {
+                    Assert.Equal(forward[i], gen.PreviousULong());
+                }
+            }
+        }
+    }
+}
