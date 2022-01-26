64f668c068e78029757666cf2518b1310eabb8b5
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-01-25 20:41:11 -0800

    Add MixMX for maximum mixing power.

--- ShaiRandom/Mixers.cs
+++ ShaiRandom/Mixers.cs
@@ -42,5 +42,31 @@
             return (state = ((state = (state ^ state.RotateLeft(39) ^ state.RotateLeft(17)) * 0x9E6C63D0676A9A99L) ^ state >> 23 ^ state >> 51) * 0x9E6D62D06F6A9A9BUL) ^ state >> 23 ^ state >> 51;
         }
 
+        /// <summary>
+        /// A very-high-quality mixing method that takes its state as a parameter; state is expected to change between calls to this.
+        /// </summary>
+        /// <remarks>
+        /// It is suggested that you use <code>Mixers.MixMX(++state)</code> to produce a sequence of different numbers. You can instead use any odd increment,
+        /// but smaller ones are preferred because there is probably a very large constant that is close to the modular multiplicative inverse of the one constant
+        /// multiplier this uses, and using that constant as an increment might cause problems.
+        /// </remarks>
+        /// <br/>
+        /// This uses Jon Kagstrom's <a href="http://jonkagstrom.com/mx3/mx3_rev2.html">Revised MX3 Mixer</a>, which seems exceedingly robust even after long runs of
+        /// the brutally-difficult remortality test.
+        /// <param name="state">Any ulong; subsequent calls should change by an odd number, such as with <code>Mixers.MixMX(++state)</code>.</param>
+        /// <returns>Any ulong.</returns>
+        public static ulong MixMX(ulong state)
+        {
+            state ^= state >> 32;
+            state *= 0xBEA225F9EB34556DUL;
+            state ^= state >> 29;
+            state *= 0xBEA225F9EB34556DUL;
+            state ^= state >> 32;
+            state *= 0xBEA225F9EB34556DUL;
+            state ^= state >> 29;
+            return state;
+        }
+    }
+
     }
 }
