79b913bac04010f79fbb3539d998d5d974762dcc
Chris3606 <cridley106@gmail.com>
2022-02-05 20:25:22 -0600

    Documented MinRandom.  Fixed bug with its NextExclusiveDecimal implementation.

--- ShaiRandom/Generators/MinRandom.cs
+++ ShaiRandom/Generators/MinRandom.cs
@@ -114,10 +114,26 @@ namespace ShaiRandom.Generators
         /// <returns><see cref="long.MinValue"/>.</returns>
         public long NextLong() => long.MinValue;
 
+        /// <summary>
+        /// Returns the minimum of 0 and the defined bound (considering <paramref name="bound"/> to be exclusive).
+        /// </summary>
+        /// <param name="bound"/>
+        /// <returns>The minimum of 0 and the defined bound (considering <paramref name="bound"/> to be exclusive)</returns>
         public ulong NextULong(ulong bound) => NextULong(0, bound);
 
+        /// <summary>
+        /// Returns the minimum of 0 and the defined bound (considering <paramref name="outerBound"/> to be exclusive).
+        /// </summary>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of 0 and the defined bound (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public long NextLong(long outerBound) => NextLong(0, outerBound);
 
+        /// <summary>
+        /// Returns the minimum of the defined bounds (considering <paramref name="outer"/> to be exclusive).
+        /// </summary>
+        /// <param name="inner"/>
+        /// <param name="outer"/>
+        /// <returns>The minimum of the defined bounds (considering <paramref name="outer"/> to be exclusive)</returns>
         public ulong NextULong(ulong inner, ulong outer)
         {
             // Special case
@@ -143,8 +159,17 @@ namespace ShaiRandom.Generators
             return Math.Min(inner, outer);
         }
 
+        /// <summary>
+        /// Always returns 0 (no bits set).
+        /// </summary>
+        /// <param name="bits"/>
+        /// <returns>A number with no bits set (eg. 0)</returns>
         public uint NextBits(int bits) => 0;
 
+        /// <summary>
+        /// Fills the buffer with <see cref="byte.MinValue"/>.
+        /// </summary>
+        /// <param name="bytes">The buffer to fill.</param>
         public void NextBytes(Span<byte> bytes)
         {
             for (int i = 0; i < bytes.Length; i++)
@@ -157,8 +182,17 @@ namespace ShaiRandom.Generators
         /// <returns><see cref="int.MinValue"/>.</returns>
         public int NextInt() => int.MinValue;
 
+        /// <summary>
+        /// Always returns <see cref="uint.MinValue"/>.
+        /// </summary>
+        /// <returns><see cref="uint.MinValue"/>.</returns>
         public uint NextUInt() => uint.MinValue;
 
+        /// <summary>
+        /// Returns the minimum of 0 and the defined bound (considering <paramref name="bound"/> to be exclusive).
+        /// </summary>
+        /// <param name="bound"/>
+        /// <returns>The minimum of 0 and the defined bound (considering <paramref name="bound"/> to be exclusive)</returns>
         public uint NextUInt(uint bound) => (uint)NextULong(bound);
 
         /// <summary>
@@ -168,16 +202,57 @@ namespace ShaiRandom.Generators
         /// <returns>The minimum of 0 and the defined bound (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public int NextInt(int outerBound) => (int)NextLong(outerBound);
 
+        /// <summary>
+        /// Returns the minimum of the defined bounds (considering <paramref name="outerBound"/> to be exclusive).
+        /// </summary>
+        /// <param name="innerBound"/>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of the defined bounds (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public uint NextUInt(uint innerBound, uint outerBound) => (uint)NextULong(innerBound, outerBound);
 
+        /// <summary>
+        /// Returns the minimum of the defined bounds (considering <paramref name="outerBound"/> to be exclusive).
+        /// </summary>
+        /// <param name="innerBound"/>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of the defined bounds (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public int NextInt(int innerBound, int outerBound) => (int)NextLong(innerBound, outerBound);
 
+        /// <summary>
+        /// Always returns false.
+        /// </summary>
+        /// <returns>False</returns>
         public bool NextBool() => false;
 
+        /// <summary>
+        /// Always returns 0.0f.
+        /// </summary>
+        /// <returns>0.0f</returns>
         public float NextFloat() => 0.0f;
 
+        /// <summary>
+        /// Returns the minimum of 0.0f and the defined bound (considering <paramref name="outerBound"/> to be exclusive).
+        /// </summary>
+        /// <remarks>
+        /// In general, this function has the same characteristics of <see cref="AbstractRandom.NextFloat(float)"/>
+        /// in terms of how close it can get to given bounds, etc.  Currently, it also shares issues with the AbstractRandom
+        /// implementation which can cause it to return <paramref name="outerBound"/> inclusive with some values.
+        /// </remarks>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of 0.0f and the defined bound (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public float NextFloat(float outerBound) => NextFloat(0, outerBound);
 
+        /// <summary>
+        /// Returns the minimum of the defined bounds (considering <paramref name="outerBound"/> to be exclusive).
+        /// </summary>
+        /// <remarks>
+        /// In general, this function has the same characteristics of <see cref="AbstractRandom.NextFloat(float, float)"/>
+        /// in terms of how close it can get to given bounds, etc.  Currently, it also shares issues with the AbstractRandom
+        /// implementation which can cause it to return <paramref name="outerBound"/> inclusive with some values.
+        /// </remarks>
+        /// <param name="innerBound"/>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of the defined bounds (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public float NextFloat(float innerBound, float outerBound)
         {
             // Note: this breaks exclusivity with, for example innerBound=1.9f and outerBound=1.8f (it returns 1.8f);
@@ -186,10 +261,35 @@ namespace ShaiRandom.Generators
             return innerBound + startingVal * (outerBound - innerBound);
         }
 
+        /// <summary>
+        /// Always returns 0.0.
+        /// </summary>
+        /// <returns>0.0</returns>
         public double NextDouble() => 0.0;
 
+        /// <summary>
+        /// Returns the minimum of 0.0 and the defined bound (considering <paramref name="outerBound"/> to be exclusive).
+        /// </summary>
+        /// <remarks>
+        /// In general, this function has the same characteristics of <see cref="AbstractRandom.NextDouble(double)"/>
+        /// in terms of how close it can get to given bounds, etc.  Currently, it also shares issues with the AbstractRandom
+        /// implementation which can cause it to return <paramref name="outerBound"/> inclusive with some values.
+        /// </remarks>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of 0.0 and the defined bound (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public double NextDouble(double outerBound) => NextDouble(0, outerBound);
 
+        /// <summary>
+        /// Returns the minimum of the defined bounds (considering <paramref name="outerBound"/> to be exclusive).
+        /// </summary>
+        /// <remarks>
+        /// In general, this function has the same characteristics of <see cref="AbstractRandom.NextDouble(double, double)"/>
+        /// in terms of how close it can get to given bounds, etc.  Currently, it also shares issues with the AbstractRandom
+        /// implementation which can cause it to return <paramref name="outerBound"/> inclusive with some values.
+        /// </remarks>
+        /// <param name="innerBound"/>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of the defined bounds (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public double NextDouble(double innerBound, double outerBound)
         {
             // Note: this breaks exclusivity with, for example innerBound=1.9 and outerBound=1.8 (it returns 1.8);
@@ -198,10 +298,33 @@ namespace ShaiRandom.Generators
             return innerBound + startingVal * (outerBound - innerBound);
         }
 
+        /// <summary>
+        /// Always returns 0.0M.
+        /// </summary>
+        /// <returns>0.0M</returns>
         public decimal NextDecimal() => 0.0M;
 
+        /// <summary>
+        /// Returns the minimum of 0.0M and the defined bound (considering <paramref name="outerBound"/> to be exclusive).
+        /// </summary>
+        /// <remarks>
+        /// In general, this function has the same characteristics of <see cref="AbstractRandom.NextDecimal(decimal)"/>
+        /// in terms of how close it can get to given bounds, etc.
+        /// </remarks>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of 0.0M and the defined bound (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public decimal NextDecimal(decimal outerBound) => NextDecimal(0, outerBound);
 
+        /// <summary>
+        /// Returns the minimum of the defined bounds (considering <paramref name="outerBound"/> to be exclusive).
+        /// </summary>
+        /// <remarks>
+        /// In general, this function has the same characteristics of <see cref="AbstractRandom.NextDecimal(decimal, decimal)"/>
+        /// in terms of how close it can get to given bounds, etc.
+        /// </remarks>
+        /// <param name="innerBound"/>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of the defined bounds (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public decimal NextDecimal(decimal innerBound, decimal outerBound)
         {
             unchecked
@@ -213,28 +336,98 @@ namespace ShaiRandom.Generators
             }
         }
 
+        /// <summary>
+        /// Always returns 0.0.
+        /// </summary>
+        /// <returns>0.0</returns>
         public double NextInclusiveDouble() => 0.0;
 
+        /// <summary>
+        /// Returns the minimum of 0.0 and the defined bound.
+        /// </summary>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of 0.0 and the defined bound</returns>
         public double NextInclusiveDouble(double outerBound) => NextInclusiveDouble(0.0, outerBound);
 
+        /// <summary>
+        /// Returns the minimum of the defined bounds.
+        /// </summary>
+        /// <param name="innerBound"/>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of the defined bounds</returns>
         public double NextInclusiveDouble(double innerBound, double outerBound) => Math.Min(innerBound, outerBound);
 
+        /// <summary>
+        /// Always returns 0.0f.
+        /// </summary>
+        /// <returns>0.0f</returns>
         public float NextInclusiveFloat() => 0.0f;
 
+        /// <summary>
+        /// Returns the minimum of 0.0f and the defined bound.
+        /// </summary>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of 0.0f and the defined bound</returns>
         public float NextInclusiveFloat(float outerBound) => NextInclusiveFloat(0.0f, outerBound);
 
+        /// <summary>
+        /// Returns the minimum of the defined bounds.
+        /// </summary>
+        /// <param name="innerBound"/>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of the defined bounds</returns>
         public float NextInclusiveFloat(float innerBound, float outerBound) => MathF.Min(innerBound, outerBound);
 
+        /// <summary>
+        /// Always returns 0.0M.
+        /// </summary>
+        /// <returns>0.0M</returns>
         public decimal NextInclusiveDecimal() => 0.0M;
 
+        /// <summary>
+        /// Returns the minimum of 0.0M and the defined bound.
+        /// </summary>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of 0.0M and the defined bound</returns>
         public decimal NextInclusiveDecimal(decimal outerBound) => NextInclusiveDecimal(0.0M, outerBound);
 
+        /// <summary>
+        /// Returns the minimum of the defined bounds.
+        /// </summary>
+        /// <param name="innerBound"/>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of the defined bounds</returns>
         public decimal NextInclusiveDecimal(decimal innerBound, decimal outerBound) => Math.Min(innerBound, outerBound);
 
+        /// <summary>
+        /// Always returns 1.0842021724855044E-19.
+        /// </summary>
+        /// <returns>1.0842021724855044E-19</returns>
         public double NextExclusiveDouble() => 1.0842021724855044E-19;
 
+        /// <summary>
+        /// Returns the minimum of 1.0842021724855044E-19 and the defined bound (considering <paramref name="outerBound"/> to be exclusive).
+        /// </summary>
+        /// <remarks>
+        /// In general, this function has the same characteristics of <see cref="AbstractRandom.NextExclusiveDouble(double)"/>
+        /// in terms of how close it can get to given bounds, etc.  Currently, it also shares issues with the AbstractRandom
+        /// implementation which can cause it to return <paramref name="outerBound"/> inclusive with some values.
+        /// </remarks>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of 1.0842021724855044E-19 and the defined bound (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public double NextExclusiveDouble(double outerBound) => NextExclusiveDouble(0.0, outerBound);
 
+        /// <summary>
+        /// Returns the minimum of the defined bounds (considering both bounds to be exclusive).
+        /// </summary>
+        /// <remarks>
+        /// In general, this function has the same characteristics of <see cref="AbstractRandom.NextExclusiveDouble(double, double)"/>
+        /// in terms of how close it can get to given bounds, etc.  Currently, it also shares issues with the AbstractRandom
+        /// implementation which can cause it to return <paramref name="outerBound"/> or <paramref name="innerBound"/> inclusive with some values.
+        /// </remarks>
+        /// <param name="innerBound"/>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of the defined bounds (considering both bounds to be exclusive)</returns>
         public double NextExclusiveDouble(double innerBound, double outerBound)
         {
             // Note: this breaks exclusivity with, for example innerBound=1.9 and outerBound=1.8 (it returns 1.8), same
@@ -243,10 +436,35 @@ namespace ShaiRandom.Generators
             return innerBound + startingVal * (outerBound - innerBound);
         }
 
+        /// <summary>
+        /// Always returns 1.0842022E-19f.
+        /// </summary>
+        /// <returns>1.0842022E-19f</returns>
         public float NextExclusiveFloat() => 1.0842022E-19f;
 
+        /// <summary>
+        /// Returns the minimum of 1.0842022E-19f and the defined bound (considering <paramref name="outerBound"/> to be exclusive).
+        /// </summary>
+        /// <remarks>
+        /// In general, this function has the same characteristics of <see cref="AbstractRandom.NextExclusiveFloat(float)"/>
+        /// in terms of how close it can get to given bounds, etc.  Currently, it also shares issues with the AbstractRandom
+        /// implementation which can cause it to return <paramref name="outerBound"/> inclusive with some values.
+        /// </remarks>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of 1.0842022E-19f and the defined bound (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public float NextExclusiveFloat(float outerBound) => NextExclusiveFloat(0, outerBound);
 
+        /// <summary>
+        /// Returns the minimum of the defined bounds (considering both bounds to be exclusive).
+        /// </summary>
+        /// <remarks>
+        /// In general, this function has the same characteristics of <see cref="AbstractRandom.NextExclusiveFloat(float, float)"/>
+        /// in terms of how close it can get to given bounds, etc.  Currently, it also shares issues with the AbstractRandom
+        /// implementation which can cause it to return <paramref name="outerBound"/> or <paramref name="innerBound"/> inclusive with some values.
+        /// </remarks>
+        /// <param name="innerBound"/>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of the defined bounds (considering both bounds to be exclusive)</returns>
         public float NextExclusiveFloat(float innerBound, float outerBound)
         {
             // Note: this breaks exclusivity with, for example innerBound=1.9f and outerBound=1.8f (it returns 1.8f), same
@@ -255,23 +473,39 @@ namespace ShaiRandom.Generators
             return innerBound + startingVal * (outerBound - innerBound);
         }
 
-        public decimal NextExclusiveDecimal()
-        {
-            unchecked
-            {
-                const ulong bits = 1;
-                return new decimal(NextInt(0xFFFFFFF) + 1, (int)(bits & 0xFFFFFFFFUL), 0, false, 28);
-            }
-        }
+        /// <summary>
+        /// Always returns 0.0000000000000000000000000001M.
+        /// </summary>
+        /// <returns>0.0000000000000000000000000001M</returns>
+        public decimal NextExclusiveDecimal() => new decimal(1, 0, 0, false, 28);
 
+        /// <summary>
+        /// Returns the minimum of 0.0000000000000000000000000001M and the defined bound (considering <paramref name="outerBound"/> to be exclusive).
+        /// </summary>
+        /// <remarks>
+        /// In general, this function has the same characteristics of <see cref="AbstractRandom.NextExclusiveDecimal(decimal)"/>
+        /// in terms of how close it can get to given bounds, etc.
+        /// </remarks>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of 0.0000000000000000000000000001M and the defined bound (considering <paramref name="outerBound"/> to be exclusive)</returns>
         public decimal NextExclusiveDecimal(decimal outerBound) => NextExclusiveDecimal(0.0M, outerBound);
 
+        /// <summary>
+        /// Returns the minimum of the defined bounds (considering both bounds to be exclusive).
+        /// </summary>
+        /// <remarks>
+        /// In general, this function has the same characteristics of <see cref="AbstractRandom.NextExclusiveDecimal(decimal, decimal)"/>
+        /// in terms of how close it can get to given bounds, etc.
+        /// </remarks>
+        /// <param name="innerBound"/>
+        /// <param name="outerBound"/>
+        /// <returns>The minimum of the defined bounds (considering both bounds to be exclusive)</returns>
         public decimal NextExclusiveDecimal(decimal innerBound, decimal outerBound)
         {
             unchecked
             {
-                ulong bits = innerBound > outerBound ? 0x204fce5e3e250261UL : 1;
-                var decimalValue = new decimal(NextInt(0xFFFFFFF) + 1, (int)(bits & 0xFFFFFFFFUL), (int)(bits >> 32), false, 28);
+                ulong bits = innerBound > outerBound ? 0x204fce5e3e250261UL : 0;
+                var decimalValue = new decimal(1, (int)(bits & 0xFFFFFFFFUL), (int)(bits >> 32), false, 28);
 
                 return innerBound + decimalValue * (outerBound - innerBound);
             }
