2985619d4f0b0e5dcdb402156818708f2868c0ce
Chris3606 <cridley106@gmail.com>
2022-02-08 00:24:25 -0600

    Added custom XUnit Test Framework to allow us to register the ShaiRandom default serialization tags before we run any of our unit tests.

---
+++ ShaiRandom.UnitTests/TestFramework.cs
@@ -0,0 +1,15 @@
+﻿using Xunit.Abstractions;
+using Xunit.Sdk;
+
+[assembly: Xunit.TestFramework("ShaiRandom.UnitTests.TestFramework", "ShaiRandom.UnitTests")]
+namespace ShaiRandom.UnitTests
+{
+    public class TestFramework : XunitTestFramework
+    {
+        public TestFramework(IMessageSink messageSink)
+            :base(messageSink)
+        {
+            Serializer.RegisterShaiRandomDefaultTags();
+        }
+    }
+}
