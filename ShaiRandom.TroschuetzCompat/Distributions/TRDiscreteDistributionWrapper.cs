333a99cecfe7f86ece08abd176e3f0f6d95c5825
Chris3606 <cridley106@gmail.com>
2022-02-12 21:18:28 -0600

    Uncommented TRCompat package code, and updated it to depend on alpha05 so it will function properly.  All unit tests passing.

--- ShaiRandom.TroschuetzCompat/Distributions/TRDiscreteDistributionWrapper.cs
+++ ShaiRandom.TroschuetzCompat/Distributions/TRDiscreteDistributionWrapper.cs
@@ -3,34 +3,34 @@ using Troschuetz.Random;
 
 namespace ShaiRandom.TroschuetzCompat.Distributions
 {
-    // /// <summary>
-    // /// Wraps a ShaiRandom IEnhancedDiscreteDistribution object so it can also be used as a Troschuetz.Random IDiscreteDistribution.
-    // /// </summary>
-    // /// <remarks>
-    // /// This class implements both IDiscreteDistribution and IEnhancedDiscreteDistribution.  Any ShaiRandom distribution member is
-    // /// implemented by simply forwarding to the ShaiRandom distribution being wrapped;  all Troschuetz methods are
-    // /// explicitly implemented and are implemented in terms of ShaiRandom distribution methods.
-    // /// </remarks>
-    // public class TRDiscreteDistributionWrapper : TRDistributionWrapper, IDiscreteDistribution, IEnhancedDiscreteDistribution
-    // {
-    //     /// <summary>
-    //     /// The ShaiRandom distribution being wrapped.
-    //     /// </summary>
-    //     public new IEnhancedDiscreteDistribution Wrapped => (IEnhancedDiscreteDistribution)base.Wrapped;
-    //
-    //     /// <summary>
-    //     /// Creates a new wrapper which wraps the given ShaiRandom distribution.
-    //     /// </summary>
-    //     /// <param name="wrapped">The ShaiRandom distribution to wrap.</param>
-    //     public TRDiscreteDistributionWrapper(IEnhancedDiscreteDistribution wrapped)
-    //         : base(wrapped)
-    //     { }
-    //
-    //     /// <inheritdoc />
-    //     public int NextInt() => Wrapped.NextInt();
-    //
-    //     #region IDiscreteDistribution Explicit Implementation
-    //     int IDiscreteDistribution.Next() => NextInt();
-    //     #endregion
-    // }
+    /// <summary>
+    /// Wraps a ShaiRandom IEnhancedDiscreteDistribution object so it can also be used as a Troschuetz.Random IDiscreteDistribution.
+    /// </summary>
+    /// <remarks>
+    /// This class implements both IDiscreteDistribution and IEnhancedDiscreteDistribution.  Any ShaiRandom distribution member is
+    /// implemented by simply forwarding to the ShaiRandom distribution being wrapped;  all Troschuetz methods are
+    /// explicitly implemented and are implemented in terms of ShaiRandom distribution methods.
+    /// </remarks>
+    public class TRDiscreteDistributionWrapper : TRDistributionWrapper, IDiscreteDistribution, IEnhancedDiscreteDistribution
+    {
+        /// <summary>
+        /// The ShaiRandom distribution being wrapped.
+        /// </summary>
+        public new IEnhancedDiscreteDistribution Wrapped => (IEnhancedDiscreteDistribution)base.Wrapped;
+
+        /// <summary>
+        /// Creates a new wrapper which wraps the given ShaiRandom distribution.
+        /// </summary>
+        /// <param name="wrapped">The ShaiRandom distribution to wrap.</param>
+        public TRDiscreteDistributionWrapper(IEnhancedDiscreteDistribution wrapped)
+            : base(wrapped)
+        { }
+
+        /// <inheritdoc />
+        public int NextInt() => Wrapped.NextInt();
+
+        #region IDiscreteDistribution Explicit Implementation
+        int IDiscreteDistribution.Next() => NextInt();
+        #endregion
+    }
 }
