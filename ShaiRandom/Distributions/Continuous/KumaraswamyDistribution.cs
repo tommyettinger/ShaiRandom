8fc504fbccb6b00e3b902f6f56977e9d5ba8a668
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-02-25 19:28:26 -0800

    Bring Kumaraswamy to dists.
    This was the only distribution that I had implemented as an IEnhancedDistribution, but wasn't in Troschuetz.Random .

---
+++ ShaiRandom/Distributions/Continuous/KumaraswamyDistribution.cs
@@ -0,0 +1,322 @@
+﻿// Copyright (c) Alessio Parma <alessio.parma@gmail.com>. All rights reserved.
+//
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+namespace ShaiRandom.Distributions.Continuous
+{
+    using System;
+    using System.Diagnostics;
+    using ShaiRandom;
+    using ShaiRandom.Generators;
+
+    /// <summary>
+    ///   Provides generation of Kumaraswamy distributed random numbers.
+    /// </summary>
+    /// <remarks>
+    ///   <para>
+    ///     The implementation of the <see cref="KumaraswamyDistribution"/> type bases upon information
+    ///     presented on <a href="http://en.wikipedia.org/wiki/Kumaraswamy_distribution">Wikipedia - Kumaraswamy distribution</a>.
+    ///   </para>
+    ///   <para>The thread safety of this class depends on the one of the underlying generator.</para>
+    /// </remarks>
+    [Serializable]
+    public sealed class KumaraswamyDistribution : AbstractDistribution, IContinuousDistribution, IAlphaDistribution<double>, IBetaDistribution<double>
+    {
+        #region Constants
+
+        /// <summary>
+        ///   The default value assigned to <see cref="Alpha"/> if none is specified.
+        /// </summary>
+        public const double DefaultAlpha = 2.0;
+
+        /// <summary>
+        ///   The default value assigned to <see cref="Beta"/> if none is specified.
+        /// </summary>
+        public const double DefaultBeta = 2.0;
+
+        #endregion Constants
+
+        #region Fields
+
+        /// <summary>
+        ///   Stores the parameter alpha which is used for generation of Kumaraswamy distributed random numbers.
+        /// </summary>
+        private double _alpha;
+
+        /// <summary>
+        ///   Stores the parameter beta which is used for generation of Kumaraswamy distributed random numbers.
+        /// </summary>
+        private double _beta;
+
+        /// <summary>
+        ///   Gets or sets the parameter alpha which is used for generation of Kumaraswamy distributed
+        ///   random numbers.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///   <paramref name="value"/> is less than or equal to zero.
+        /// </exception>
+        /// <remarks>
+        ///   Calls <see cref="AreValidParams"/> to determine whether a value is valid and therefore assignable.
+        /// </remarks>
+        public double Alpha
+        {
+            get { return 1.0 / _alpha; }
+            set
+            {
+                if (!IsValidAlpha(value)) throw new ArgumentOutOfRangeException(nameof(Alpha), ErrorMessages.InvalidParams);
+                _alpha = 1.0 / value;
+            }
+        }
+
+        /// <summary>
+        ///   Gets or sets the parameter beta which is used for generation of Kumaraswamy distributed
+        ///   random numbers.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///   <paramref name="value"/> is less than or equal to zero.
+        /// </exception>
+        /// <remarks>
+        ///   Calls <see cref="AreValidParams"/> to determine whether a value is valid and therefore assignable.
+        /// </remarks>
+        public double Beta
+        {
+            get { return 1.0 / _beta; }
+            set
+            {
+                if (!IsValidBeta(value)) throw new ArgumentOutOfRangeException(nameof(Beta), ErrorMessages.InvalidParams);
+                _beta = 1.0 / value;
+            }
+        }
+
+        #endregion Fields
+
+        #region Construction
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="KumaraswamyDistribution"/> class, using a
+        ///   <see cref="TrimRandom"/> as underlying random number generator.
+        /// </summary>
+        public KumaraswamyDistribution()
+            : this(new TrimRandom(), DefaultAlpha, DefaultBeta)
+        {
+            Debug.Assert(Generator is TrimRandom);
+            Debug.Assert(Equals(Alpha, DefaultAlpha));
+            Debug.Assert(Equals(Beta, DefaultBeta));
+        }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="KumaraswamyDistribution"/> class, using a
+        ///   <see cref="TrimRandom"/> with the specified seed value.
+        /// </summary>
+        /// <param name="seed">
+        ///   An unsigned number used to calculate a starting value for the pseudo-random number sequence.
+        /// </param>
+        public KumaraswamyDistribution(uint seed)
+            : this(new TrimRandom(seed), DefaultAlpha, DefaultBeta)
+        {
+            Debug.Assert(Generator is TrimRandom);
+            Debug.Assert(Equals(Alpha, DefaultAlpha));
+            Debug.Assert(Equals(Beta, DefaultBeta));
+        }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="KumaraswamyDistribution"/> class, using the
+        ///   specified <see cref="IEnhancedRandom"/> as underlying random number generator.
+        /// </summary>
+        /// <param name="generator">An <see cref="IEnhancedRandom"/> object.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="generator"/> is <see langword="null"/>.</exception>
+        public KumaraswamyDistribution(IEnhancedRandom generator)
+            : this(generator, DefaultAlpha, DefaultBeta)
+        {
+            Debug.Assert(ReferenceEquals(Generator, generator));
+            Debug.Assert(Equals(Alpha, DefaultAlpha));
+            Debug.Assert(Equals(Beta, DefaultBeta));
+        }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="KumaraswamyDistribution"/> class, using a
+        ///   <see cref="TrimRandom"/> as underlying random number generator.
+        /// </summary>
+        /// <param name="alpha">
+        ///   The parameter alpha which is used for generation of Kumaraswamy distributed random numbers.
+        /// </param>
+        /// <param name="beta">
+        ///   The parameter beta which is used for generation of Kumaraswamy distributed random numbers.
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///   <paramref name="alpha"/> or <paramref name="beta"/> are less than or equal to zero.
+        /// </exception>
+        public KumaraswamyDistribution(double alpha, double beta)
+            : this(new TrimRandom(), alpha, beta)
+        {
+            Debug.Assert(Generator is TrimRandom);
+            Debug.Assert(Equals(Alpha, alpha));
+            Debug.Assert(Equals(Beta, beta));
+        }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="KumaraswamyDistribution"/> class, using a
+        ///   <see cref="TrimRandom"/> with the specified seed value.
+        /// </summary>
+        /// <param name="seed">
+        ///   An unsigned number used to calculate a starting value for the pseudo-random number sequence.
+        /// </param>
+        /// <param name="alpha">
+        ///   The parameter alpha which is used for generation of Kumaraswamy distributed random numbers.
+        /// </param>
+        /// <param name="beta">
+        ///   The parameter beta which is used for generation of Kumaraswamy distributed random numbers.
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///   <paramref name="alpha"/> or <paramref name="beta"/> are less than or equal to zero.
+        /// </exception>
+        public KumaraswamyDistribution(uint seed, double alpha, double beta)
+            : this(new TrimRandom(seed), alpha, beta)
+        {
+            Debug.Assert(Generator is TrimRandom);
+            Debug.Assert(Equals(Alpha, alpha));
+            Debug.Assert(Equals(Beta, beta));
+        }
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="KumaraswamyDistribution"/> class, using the
+        ///   specified <see cref="IEnhancedRandom"/> as underlying random number generator.
+        /// </summary>
+        /// <param name="generator">An <see cref="IEnhancedRandom"/> object.</param>
+        /// <param name="alpha">
+        ///   The parameter alpha which is used for generation of Kumaraswamy distributed random numbers.
+        /// </param>
+        /// <param name="beta">
+        ///   The parameter beta which is used for generation of Kumaraswamy distributed random numbers.
+        /// </param>
+        /// <exception cref="ArgumentNullException"><paramref name="generator"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///   <paramref name="alpha"/> or <paramref name="beta"/> are less than or equal to zero.
+        /// </exception>
+        public KumaraswamyDistribution(IEnhancedRandom generator, double alpha, double beta) : base(generator)
+        {
+            var vp = AreValidParams;
+            if (!vp(alpha, beta)) throw new ArgumentOutOfRangeException($"{nameof(alpha)}/{nameof(beta)}", ErrorMessages.InvalidParams);
+            Alpha = alpha;
+            Beta = beta;
+        }
+
+        #endregion Construction
+
+        #region Instance Methods
+
+        /// <summary>
+        ///   Determines whether the specified value is valid for parameter <see cref="Alpha"/>.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <returns><see langword="true"/> if value is greater than 0.0; otherwise, <see langword="false"/>.</returns>
+        public bool IsValidAlpha(double value) => AreValidParams(value, Beta);
+
+        /// <summary>
+        ///   Determines whether the specified value is valid for parameter <see cref="Beta"/>.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <returns><see langword="true"/> if value is greater than 0.0; otherwise, <see langword="false"/>.</returns>
+        public bool IsValidBeta(double value) => AreValidParams(Alpha, value);
+
+        #endregion Instance Methods
+
+        #region IContinuousDistribution Members
+
+        /// <summary>
+        ///   Gets the maximum possible value of distributed random numbers.
+        /// </summary>
+        public double Maximum => 1.0;
+
+        /// <summary>
+        ///   Gets the mean of distributed random numbers.
+        /// </summary>
+        /// <exception cref="NotSupportedException">
+        ///   Thrown if mean is not defined for given distribution with some parameters.
+        /// </exception>
+        public double Mean
+        {
+            get
+            {
+                double b = 1.0 / _beta;
+                return (MathUtils.Gamma(1.0 + _alpha) * MathUtils.Gamma(b) / b) / MathUtils.Gamma(1.0 + _alpha + b);
+            }
+        }
+
+        /// <summary>
+        ///   Gets the median of distributed random numbers.
+        /// </summary>
+        /// <exception cref="NotSupportedException">
+        ///   Thrown if median is not defined for given distribution with some parameters.
+        /// </exception>
+        public double Median => Math.Pow(1.0 - Math.Pow(2.0, -_beta), _alpha);
+
+        /// <summary>
+        ///   Gets the minimum possible value of distributed random numbers.
+        /// </summary>
+        public double Minimum => 0.0;
+
+        /// <summary>
+        ///   Gets the mode of distributed random numbers.
+        /// </summary>
+        /// <exception cref="NotSupportedException">
+        ///   Thrown if mode is not defined for given distribution with some parameters.
+        /// </exception>
+        public double[] Mode
+        {
+            get
+            {
+                if (_alpha <= 1.0 && _beta <= 1.0 && (_alpha == 1.0 && _beta == 1.0))
+                {
+                    return new[] { Math.Pow((Alpha - 1.0) / (Alpha * Beta - 1.0), _alpha) };
+                }
+                throw new NotSupportedException(ErrorMessages.UndefinedModeForParams);
+            }
+        }
+
+        /// <summary>
+        ///   Gets the variance of distributed random numbers.
+        /// </summary>
+        /// <exception cref="NotSupportedException">
+        ///   Thrown if variance is not defined for given distribution with some parameters.
+        /// </exception>
+        public double Variance
+        {
+            get { throw new NotSupportedException(ErrorMessages.UndefinedVariance); }
+        }
+
+        /// <summary>
+        ///   Returns a distributed floating point random number.
+        /// </summary>
+        /// <returns>A distributed double-precision floating point number.</returns>
+        public double NextDouble() => Sample(Generator, _alpha, _beta);
+
+        #endregion IContinuousDistribution Members
+
+        #region TRandom Helpers
+
+        /// <summary>
+        ///   Determines whether Kumaraswamy distribution is defined under given parameters. The default
+        ///   definition returns true if alpha and beta are greater than zero; otherwise, it returns false.
+        /// </summary>
+        /// <remarks>
+        ///   This is an extensibility point for the <see cref="KumaraswamyDistribution"/> class.
+        /// </remarks>
+        public static Func<double, double, bool> AreValidParams { get; set; } = (alpha, beta) =>
+        {
+            return alpha > 0.0 && beta > 0.0;
+        };
+
+        /// <summary>
+        ///   Declares a function returning a Kumaraswamy distributed floating point random number.
+        /// </summary>
+        /// <remarks>
+        ///   The alpha and beta parameters to the returned Func are expected to be pre-inverted (equal to 1.0 / Alpha or 1.0 / Beta), which
+        ///   is how this class stores alpha and beta internally.
+        ///   This is an extensibility point for the <see cref="KumaraswamyDistribution"/> class.
+        /// </remarks>
+        public static Func<IEnhancedRandom, double, double, double> Sample { get; set; } = (generator, alpha, beta) => Math.Pow(1.0 - Math.Pow(generator.NextExclusiveDouble(), beta), alpha);
+
+        #endregion TRandom Helpers
+    }
+}
