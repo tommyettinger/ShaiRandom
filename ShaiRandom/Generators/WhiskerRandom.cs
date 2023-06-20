028c076e4c4780f6ae44d5f25ea25e8b8586ff01
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-06-19 20:42:38 -0700

    Bring over WhiskerRandom from Juniper.

---
+++ ShaiRandom/Generators/WhiskerRandom.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ShaiRandom.Generators
+{
+    /// <summary>
+    /// It's a simpler AbstractRandom with 4 states, built around a "chaotic" construction with no guarantee of a minimum period, but likely to be a very long one.
+    /// </summary>
+    /// <remarks>
+    /// Tommy Ettinger:
+    /// This is "simpler" because it has four states and performs four math operations per update (a multiplication, a bitwise rotation, an add, and a XOR), with a good
+    /// compiler able to handle those as instruction-level parallel operations.
+    /// 
+    /// This is the fastest generator I've written so far... when run on on the JVM. It isn't as fast on .NET (any version), because smaller state sizes do better here.
+    /// It's still very high-quality, passing pretty much any test for non-cryptographic randomness I've thrown at it. That includes 64TB of PractRand without anomalies,
+    /// 179 PB of ReMortality without suspect results, and generally frequent usage throughout my other code. Not having a guarantee of a minimum period is sometimes a
+    /// concern if a generator would be used for many generations, but the odds of encountering a period less than, say, a billion are effectively too small to measure.
+    /// This is the same argument RomuTrio makes to justify its unknown minimum period but probable high actual period, but unlike RomuTrio, WhiskerRandom doesn't have
+    /// a glaringly-obvious issue when all states are 0.
+    /// </remarks>
+    public sealed class WhiskerRandom : AbstractRandom
+    {
+        /// <summary>
+        /// The identifying tag here is "WhiR" .
+        /// </summary>
+        public override string DefaultTag => "WhiR";
+        
+        /// <summary>
+        /// The first state; can be any ulong.
+        /// </summary>
+        public ulong StateA { get; set; }
+        /// <summary>
+        /// The second state; can be any ulong.
+        /// </summary>
+        public ulong StateB { get; set; }
+        /// <summary>
+        /// The third state; can be any ulong.
+        /// </summary>
+        public ulong StateC { get; set; }
+        /// <summary>
+        /// The fourth state; can be any ulong.
+        /// </summary>
+        /// <remarks>If this has just been set to some value, then the next call to <see cref="NextULong()">NextUlong()</see> will return that value as-is. Later calls will be more random.</remarks>
+        public ulong StateD { get; set; }
+
+        /// <summary>
+        /// Creates a new WhiskerRandom with a random state.
+        /// </summary>
+        public WhiskerRandom()
+        {
+            StateA = MakeSeed();
+            StateB = MakeSeed();
+            StateC = MakeSeed();
+            StateD = MakeSeed();
+        }
+
+        /// <summary>
+        /// Creates a new WhiskerRandom with the given seed; all ulong values are permitted.
+        /// </summary>
+        /// <remarks>
+        /// The seed will be passed to <see cref="SetSeed(WhiskerRandom, ulong)">SetSeed(WhiskerRandom, ulong)</see>, to attempt to adequately distribute the seed randomly.
+        /// </remarks>
+        /// <param name="seed">Any ulong.</param>
+        public WhiskerRandom(ulong seed)
+        {
+            SetSeed(this, seed);
+        }
+
+        /// <summary>
+        /// Creates a new WhiskerRandom with the given four states; all ulong values are permitted.
+        /// </summary>
+        /// <remarks>
+        /// The states will be used verbatim.
+        /// </remarks>
+        /// <param name="stateA">Any ulong.</param>
+        /// <param name="stateB">Any ulong.</param>
+        /// <param name="stateC">Any ulong.</param>
+        /// <param name="stateD">Any ulong.</param>
+        public WhiskerRandom(ulong stateA, ulong stateB, ulong stateC, ulong stateD)
+        {
+            StateA = stateA;
+            StateB = stateB;
+            StateC = stateC;
+            StateD = stateD;
+        }
+
+        /// <summary>
+        /// This generator has 4 ulong states, so this returns 4.
+        /// </summary>
+        public override int StateCount => 4;
+        /// <summary>
+        /// This supports <see cref="SelectState(int)"/>.
+        /// </summary>
+        public override bool SupportsReadAccess => true;
+        /// <summary>
+        /// This supports <see cref="SetSelectedState(int, ulong)"/>.
+        /// </summary>
+        public override bool SupportsWriteAccess => true;
+        /// <summary>
+        /// This does not support <see cref="IEnhancedRandom.Skip(ulong)"/>.
+        /// </summary>
+        public override bool SupportsSkip => false;
+        /// <summary>
+        /// This does not support <see cref="IEnhancedRandom.Leap()"/>.
+        /// </summary>
+        public override bool SupportsLeap => false;
+        /// <summary>
+        /// This supports <see cref="PreviousULong()"/>.
+        /// </summary>
+        public override bool SupportsPrevious => true;
+
+        /// <summary>
+        /// Gets the state determined by selection, as-is.
+        /// </summary>
+        /// <remarks>The value for selection should be between 0 and 3, inclusive; if it is any other value this gets state D as if 3 was given.</remarks>
+        /// <param name="selection">used to select which state variable to get; generally 0, 1, 2, or 3.</param>
+        /// <returns>The value of the selected state.</returns>
+        public override ulong SelectState(int selection)
+        {
+            switch (selection)
+            {
+                case 0:
+                    return StateA;
+                case 1:
+                    return StateB;
+                case 2:
+                    return StateC;
+                default:
+                    return StateD;
+            }
+        }
+
+        /// <summary>
+        /// Sets one of the states, determined by selection, to value, as-is.
+        /// </summary>
+        /// <remarks>
+        /// Selections 0, 1, 2, and 3 refer to states A, B, C, and D,  and if the selection is anything else, this treats it as 3 and sets stateD.
+        /// </remarks>
+        /// <param name="selection">Used to select which state variable to set; generally 0, 1, 2, or 3.</param>
+        /// <param name="value">The exact value to use for the selected state, if valid.</param>
+        public override void SetSelectedState(int selection, ulong value)
+        {
+            switch (selection)
+            {
+                case 0:
+                    StateA = value;
+                    break;
+                case 1:
+                    StateB = value;
+                    break;
+                case 2:
+                    StateC = value;
+                    break;
+                default:
+                    StateD = value;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Initializes all 4 states of the generator to random values based on the given seed.
+        /// </summary>
+        /// <remarks>
+        /// (2 to the 64) possible initial generator states can be produced here, all with a different first value returned
+        /// by <see cref="NextULong()">NextUlong()</see> (because stateD is guaranteed to be different for every different seed).
+        /// </remarks>
+        /// <param name="seed">The initial seed; may be any ulong.</param>
+        public override void Seed(ulong seed) => SetSeed(this, seed);
+
+        private static void SetSeed(WhiskerRandom rng, ulong seed)
+        {
+            unchecked
+            {
+                rng.StateA = seed ^ 0xC6BC279692B5C323UL;
+                rng.StateB = seed ^ ~0xC6BC279692B5C323UL;
+                seed ^= seed >> 32;
+                seed *= 0xBEA225F9EB34556DUL;
+                seed ^= seed >> 29;
+                seed *= 0xBEA225F9EB34556DUL;
+                seed ^= seed >> 32;
+                seed *= 0xBEA225F9EB34556DUL;
+                seed ^= seed >> 29;
+                rng.StateC = ~seed;
+                rng.StateD = seed;
+            }
+        }
+
+        /// <summary>
+        /// Sets the state completely to the given four state variables.
+        /// </summary>
+        /// <remarks>
+        /// This is the same as setting StateA, StateB, StateC, and StateD as a group.
+        /// You may want to call <see cref="NextULong()">NextUlong()</see> a few times after setting the states like this, unless
+        /// the value for stateD (in particular) is already adequately random; the first call to NextULong(), if it is made immediately after calling this, will return stateD as-is.
+        /// </remarks>
+        /// <param name="stateA">The first state; can be any ulong.</param>
+        /// <param name="stateB">The second state; can be any ulong.</param>
+        /// <param name="stateC">The third state; can be any ulong.</param>
+        /// <param name="stateD">The fourth state; this will be returned as-is if the next call is to NextULong().</param>
+        public override void SetState(ulong stateA, ulong stateB, ulong stateC, ulong stateD)
+        {
+            StateA = stateA;
+            StateB = stateB;
+            StateC = stateC;
+            StateD = stateD;
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override ulong NextULong()
+        {
+            ulong fa = StateA;
+            ulong fb = StateB;
+            ulong fc = StateC;
+            ulong fd = StateD;
+            unchecked
+            {
+                StateA = fd * 0xF1357AEA2E62A9C5UL; // Considered good by Steele and Vigna, https://arxiv.org/abs/2001.05304v1
+                StateB = fa.RotateLeft(44);
+                StateC = fb + 0x9E3779B97F4A7C15UL; // 2 to the 64 divided by the golden ratio
+                StateD = fa ^ fc;
+            }
+            return StateD;
+        }
+
+        /// <inheritdoc />
+        public override float NextSparseFloat()
+        {
+            ulong fa = StateA;
+            ulong fb = StateB;
+            ulong fc = StateC;
+            ulong fd = StateD;
+            unchecked
+            {
+                StateA = fd * 0xF1357AEA2E62A9C5UL;
+                StateB = fa.RotateLeft(44);
+                StateC = fb + 0x9E3779B97F4A7C15UL;
+                StateD = fa ^ fc;
+                return BitConverter.Int32BitsToSingle((int)(StateD >> 41) | 0x3F800000) - 1f;
+            }
+        }
+
+        /// <inheritdoc />
+        public override double NextSparseDouble()
+        {
+            ulong fa = StateA;
+            ulong fb = StateB;
+            ulong fc = StateC;
+            ulong fd = StateD;
+            unchecked
+            {
+                StateA = fd * 0xF1357AEA2E62A9C5UL;
+                StateB = fa.RotateLeft(44);
+                StateC = fb + 0x9E3779B97F4A7C15UL;
+                StateD = fa ^ fc;
+                return BitConverter.Int64BitsToDouble((long)(StateD >> 12) | 0x3FF0000000000000L) - 1.0;
+            }
+        }
+
+        /// <inheritdoc />
+        public override ulong PreviousULong()
+        {
+            ulong fa = StateA;
+            ulong fb = StateB;
+            ulong fc = StateC;
+            ulong fd = StateD;
+            unchecked
+            {
+                StateA = fb.RotateRight(44);
+                StateB = fc - 0x9E3779B97F4A7C15UL;
+                StateC = StateA ^ fd;
+                StateD = fa * 0x781494A55DAAED0DUL; // modular multiplicative inverse of 0xF1357AEA2E62A9C5L
+                return StateA ^ StateC;
+            }
+        }
+
+        /// <inheritdoc />
+        public override IEnhancedRandom Copy() => new WhiskerRandom(StateA, StateB, StateC, StateD);
+    }
+}
