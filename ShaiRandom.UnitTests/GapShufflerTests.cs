c4ef01c9786c71d5a3b736f1a1d6b0dc100c0b91
Chris3606 <cridley106@gmail.com>
2023-07-17 10:14:30 -0500

    Finished improving unit tests and avoided some unneeded code duplication.

--- ShaiRandom.UnitTests/GapShufflerTests.cs
+++ ShaiRandom.UnitTests/GapShufflerTests.cs
@@ -2,123 +2,130 @@
 using System.Linq;
 using ShaiRandom.Generators;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace ShaiRandom.UnitTests
 {
     public class GapShufflerTests
     {
-        private readonly ITestOutputHelper _output;
-        private readonly IEnhancedRandom _rng;
-
-        public GapShufflerTests(ITestOutputHelper output)
-        {
-            _output = output;
-            _rng = new MizuchiRandom(1);
-        }
         [Fact]
         public void GapShuffleIEnumerable()
         {
+            var rng = new MizuchiRandom(1);
+
             var originalList = new[] { 1, 2, 3, 4, 5 };
             var list = new[] { 1, 2, 3, 4, 5 };
-            var shuffler = _rng.GapShuffler(list);
+            var shuffler = rng.GapShuffler(list);
 
             var shuffled = shuffler.Take(10).ToArray();
-            var halves = new[] { shuffled[..5], shuffled[5..] };
-            foreach (var half in halves)
-            {
-                // Each set of 5 contains each element of the original precisely once
-                Assert.Equal(list.ToHashSet(), half.ToHashSet());
+            CheckResult(originalList, list, shuffled, false);
 
-                // No set is the original list.  This is not _technically_ enforced by the algorithm, but it's
-                // highly unlikely to happen by chance (and won't happen with the particular RNG seed we're using).
-                // It is useful to check this to ensure all streams are randomized, to avoid bugs like the list reverting
-                // to the original one after one set of all of its elements or some such.
-                Assert.NotEqual(list, half);
-            }
-
-            // Ensure the original list was not modified (non in-place versions should copy)
-            Assert.Equal(originalList, list);
+            shuffled = GetShuffledWithNext(shuffler);
+            CheckResult(originalList, list, shuffled, false);
         }
 
         [Fact]
         public void GapShuffleSpan()
         {
+            var rng = new MizuchiRandom(1);
+
             var originalList = new[] { 1, 2, 3, 4, 5 };
             var list = new[] { 1, 2, 3, 4, 5 };
-            var shuffler = _rng.GapShuffler<int>(list.AsSpan());
+            var shuffler = rng.GapShuffler<int>(list.AsSpan());
 
             var shuffled = shuffler.Take(10).ToArray();
-            var halves = new[] { shuffled[..5], shuffled[5..] };
-            foreach (var half in halves)
-            {
-                // Each set of 5 contains each element of the original precisely once
-                Assert.Equal(list.ToHashSet(), half.ToHashSet());
+            CheckResult(originalList, list, shuffled, false);
 
-                // No set is the original list.  This is not _technically_ enforced by the algorithm, but it's
-                // highly unlikely to happen by chance (and won't happen with the particular RNG seed we're using).
-                // It is useful to check this to ensure all streams are randomized, to avoid bugs like the list reverting
-                // to the original one after one set of all of its elements or some such.
-                Assert.NotEqual(list, half);
-            }
-
-            // Ensure the original list was not modified (non in-place versions should copy)
-            Assert.Equal(originalList, list);
+            shuffled = GetShuffledWithNext(shuffler);
+            CheckResult(originalList, list, shuffled, false);
         }
 
         [Fact]
         public void GapShuffleMemory()
         {
+            var rng = new MizuchiRandom(1);
+
             var originalList = new[] { 1, 2, 3, 4, 5 };
             var list = new[] { 1, 2, 3, 4, 5 };
-            var shuffler = _rng.GapShuffler<int>(list.AsMemory());
+            var shuffler = rng.GapShuffler<int>(list.AsMemory());
 
             var shuffled = shuffler.Take(10).ToArray();
-            var halves = new[] { shuffled[..5], shuffled[5..] };
-            foreach (var half in halves)
-            {
-                // Each set of 5 contains each element of the original precisely once
-                Assert.Equal(list.ToHashSet(), half.ToHashSet());
+            CheckResult(originalList, list, shuffled, false);
 
-                // No set is the original list.  This is not _technically_ enforced by the algorithm, but it's
-                // highly unlikely to happen by chance (and won't happen with the particular RNG seed we're using).
-                // It is useful to check this to ensure all streams are randomized, to avoid bugs like the list reverting
-                // to the original one after one set of all of its elements or some such.
-                Assert.NotEqual(list, half);
-            }
-
-            // Ensure the original list was not modified (non in-place versions should copy)
-            Assert.Equal(originalList, list);
+            shuffled = GetShuffledWithNext(shuffler);
+            CheckResult(originalList, list, shuffled, false);
         }
 
         [Fact]
-        public void GapShuffleInPlace()
+        public void GapShuffleInPlaceIList()
         {
+            var rng = new MizuchiRandom(1);
+
             var originalList = new[] { 1, 2, 3, 4, 5 };
             var list = new[] { 1, 2, 3, 4, 5 };
-            var shuffler = _rng.InPlaceGapShuffler(list);
+            var shuffler = rng.InPlaceGapShuffler(list);
 
             var shuffled = shuffler.Take(10).ToArray();
-            var halves = new[] { shuffled[..5], shuffled[5..] };
-            foreach (var half in halves)
-            {
-                // Each set of 5 contains each element of the original precisely once
-                Assert.Equal(originalList.ToHashSet(), half.ToHashSet());
-
-                // No set is the original list.  This is not _technically_ enforced by the algorithm, but it's
-                // highly unlikely to happen by chance (and won't happen with the particular RNG seed we're using).
-                // It is useful to check this to ensure all streams are randomized, to avoid bugs like the list reverting
-                // to the original one after one set of all of its elements or some such.
-                Assert.NotEqual(originalList, half);
-            }
-
-            // We'll also check that the original list is modified.  Again, this is not _technically_ enforced by the algorithm,
-            // but is guaranteed to happen with the seed we chose for this test, and ensures that the implementation
-            // is shuffling in-place.
-            Assert.NotEqual(originalList, list);
+            CheckResult(originalList, list, shuffled, true);
         }
 
-        private void CheckResult(int[] originalList, int[] currentList, int[] enumerableToArray, bool inPlace)
+        [Fact]
+        public void GapShuffleInPlaceNextIList()
+        {
+            var rng = new MizuchiRandom(1);
+
+            var originalList = new[] { 1, 2, 3, 4, 5 };
+            var list = new[] { 1, 2, 3, 4, 5 };
+            var shuffler = rng.InPlaceGapShuffler(list);
+
+            var shuffled = GetShuffledWithNext(shuffler);
+            CheckResult(originalList, list, shuffled, true);
+        }
+
+        [Fact]
+        public void GapShuffleInPlaceMemory()
+        {
+            var rng = new MizuchiRandom(1);
+
+            var originalList = new[] { 1, 2, 3, 4, 5 };
+            var list = new[] { 1, 2, 3, 4, 5 };
+            var shuffler = rng.InPlaceGapShuffler(list.AsMemory());
+
+            var shuffled = shuffler.Take(10).ToArray();
+            CheckResult(originalList, list, shuffled, true);
+        }
+
+        [Fact]
+        public void GapShuffleInPlaceNextMemory()
+        {
+            var rng = new MizuchiRandom(1);
+
+            var originalList = new[] { 1, 2, 3, 4, 5 };
+            var list = new[] { 1, 2, 3, 4, 5 };
+            var shuffler = rng.InPlaceGapShuffler(list.AsMemory());
+
+            var shuffled = GetShuffledWithNext(shuffler);
+            CheckResult(originalList, list, shuffled, true);
+        }
+
+        private static int[] GetShuffledWithNext(GapShufflerEnumerator<int> shuffler)
+        {
+            var array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = shuffler.Next();
+
+            return array;
+        }
+
+        private static int[] GetShuffledWithNext(GapShufflerInPlaceMemoryEnumerator<int> shuffler)
+        {
+            var array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = shuffler.Next();
+
+            return array;
+        }
+
+        private static void CheckResult(int[] originalList, int[] currentList, int[] enumerableToArray, bool inPlace)
         {
             // Only required for how we test, not for the algorithm itself
             Assert.Equal(originalList.Length * 2, enumerableToArray.Length);
